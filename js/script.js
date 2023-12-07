c46b45a4da2d7f275df5ac124545eabf3dc4db72
Michelazani <michi.zani220@gmail.com>
2023-12-07 15:57:18 +0100

    correzione parola palindroma

--- js/script.js
+++ js/script.js
@@ -51,9 +51,34 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 E' vietato usare split(), reverse() e simili.*/
 
-const typeAWord = prompt('type a word');
+const userWord = prompt('Type a word');
 
-function isWordPalindrome = (typeAWord){
-    // if la word è palindroma>> true, se non lo è >> false
-     
+//Creo una funzione che prende come argomento una parola e restituisce la parola invertita. 
+function getReversedWord(word){
+    let reversedWord = '';
+
+    //Creo un ciclo for che scorre la parola dalla fine all'inizio, aggiungendo i caratteri alla stringa della costante reverseWord.
+    for (let i = word.length - 1; i >= 0; i--){
+        reversedWord = reversedWord + word.charAt(i);
+    }
+
+    return reversedWord;
 }
+
+//Creo una funzione che prende una parola come argomento e restituisce true se la parola è un palindromo, false se non lo è.
+function isPalindrome(word){
+    let result = true;
+    let index = 0;
+    //Creo un ciclo while che compara i caratteri iniziali della parola con quelli finali. Se trova una differenza imposta il result su false e interrompe il ciclo.
+    while ( index < Math.floor(word.length / 2) ){
+        if ( word.charAt(index) !== word.charAt(word.length - 1 - index) ){
+            result = false;
+            index = word.length;
+        }
+        index++;
+    }
+    return result;
+}
+
+//Stampo il risultato della parola inserita dall'utente utilizzando la funzione isPalindrome. Se la parola è un palindromo, verrà stampato true, altrimenti verrà stampato false.
+console.log(isPalindrome(userWord));
